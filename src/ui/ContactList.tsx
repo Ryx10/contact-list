29ee6b9abc2094aae98dad8a571026f4f786e90f
Mateusz Grzybowski <mat.grzybowski@gmail.com>
2022-07-31 20:11:33 +0200

    Add virtual list  * adjust styles for virtual list

--- src/ui/ContactList.tsx
+++ src/ui/ContactList.tsx
@@ -1,5 +1,7 @@
 import type {Contact} from '../types'
 import PersonInfo from './PersonInfo'
+import {FixedSizeList as List} from 'react-window'
+import Autosizer from 'react-virtualized-auto-sizer'
 
 import css from './ContactList.module.css'
 
@@ -16,23 +18,51 @@ const ContactList = ({
   selectContact,
   deselectContact,
 }: Props) => {
+  const items = [
+    ...selectedContacts.map((contact) => (
+      <PersonInfo
+        key={contact.id}
+        data={contact}
+        onClick={deselectContact}
+        selected
+      />
+    )),
+    ...notSelectedContacts.map((contact) => (
+      <PersonInfo key={contact.id} data={contact} onClick={selectContact} />
+    )),
+  ]
+
   return (
     <div className={css.list}>
-      {selectedContacts.map((personInfo) => (
-        <PersonInfo
-          key={personInfo.id}
-          data={personInfo}
-          onClick={deselectContact}
-          selected
-        />
-      ))}
-      {notSelectedContacts.map((personInfo) => (
-        <PersonInfo
-          key={personInfo.id}
-          data={personInfo}
-          onClick={selectContact}
-        />
-      ))}
+      <Autosizer>
+        {({height, width}: {height: number; width: number}) => (
+          <List
+            height={height}
+            itemCount={notSelectedContacts.length}
+            itemSize={175}
+            width={width}>
+            {({index, style}: {index: number; style: object}) => (
+              <div style={style}>{items[index]}</div>
+            )}
+          </List>
+        )}
+      </Autosizer>
+
+      {/*{selectedContacts.map((personInfo) => (*/}
+      {/*  <PersonInfo*/}
+      {/*    key={personInfo.id}*/}
+      {/*    data={personInfo}*/}
+      {/*    onClick={deselectContact}*/}
+      {/*    selected*/}
+      {/*  />*/}
+      {/*))}*/}
+      {/*{notSelectedContacts.map((personInfo) => (*/}
+      {/*  <PersonInfo*/}
+      {/*    key={personInfo.id}*/}
+      {/*    data={personInfo}*/}
+      {/*    onClick={selectContact}*/}
+      {/*  />*/}
+      {/*))}*/}
     </div>
   )
 }
